715df3746c6b3012c52ac420559acfb271bc2503
lentusignavus <ignavus.ii@gmail.com>
2017-02-12 09:20:09 -0600

    Adds sorting function, helper methods, and tests

--- src/utils/sortData.js
+++ src/utils/sortData.js
@@ -7,30 +7,93 @@
  * @param propertyToSort
  */
 function sort(data, propertyToSort) {
-    return data;
     // At one hundred items and a weekend project this seemed more appropriate than
     // bringing in lodash for the sort function
     // Though that would've saved me some effort
     // Good practice though
-    const length = data.length;
+    const lengthOfData = data.length;
 
-    for (var i = 0; i < length; i++) {
-        var obj = data[i];
-
+    let shouldSwapItems;
+    switch (typeof data[0][propertyToSort]) {
+        case 'string':
+            shouldSwapItems = compareStrings;
+            break;
+        case 'number':
+            shouldSwapItems = compareNumbers;
+            break;
+        default:
+            throw new Error('This function doesn\'t support sorting this type of data');
+            break;
     }
+
+    let swaps;
+
+    do {
+        swaps = 0;
+        for (let i = 0; i < lengthOfData - 1; i++) {
+            let item1 = data[i][propertyToSort];
+            let item2 = data[i + 1][propertyToSort];
+            if (shouldSwapItems(item1, item2)) {
+                swap(data, i, i + 1);
+                swaps++;
+            }
+        }
+    } while ((swaps > 0));
 }
 
 
+/**
+ * @description returns true if the first string comes after the second, lexicographically
+ * returns false if the second string comes before the first
+ * @example
+ * compareStrings("ab", "aa") === true
+ * compareStrings("ac", "ab") === false
+ *
+ * @param string1
+ * @param string2
+ * @return {boolean} - boolean representing whether or not they need to be swapped
+ */
 function compareStrings (string1, string2) {
-    const shorterStringLength = string1.length < string2.length ? string1.length : string2.length
+    const shorterStringLength = string1.length < string2.length ? string1.length : string2.length;
+    const isLongerStringFirst = string1.length > string2.length;
+    let charCode1;
+    let charCode2;
+
+    for(let i = 0; i < shorterStringLength; i++) {
+        charCode1 = string1.charCodeAt(i);
+        charCode2 = string2.charCodeAt(i);
+        if(charCode1 === charCode2) {
+            continue;
+        } else {
+            // If the character is greater then that means is comes after
+            return charCode1 > charCode2
+        }
+    }
+    // We are at the end of the shortest string
+    // So the longer string must come after the shorter, lexicographically
+    return isLongerStringFirst;
 
 }
 
-function compareNumbers () {
+/**
+ *
+ * @param num1
+ * @param num2
+ * @return {boolean} - boolean representing whether or not they need to be swapped
+ */
+function compareNumbers (num1, num2) {
+    return num1 > num2;
+}
 
+
+function swap (data, index1, index2) {
+    let temp = data[index1];
+    data[index1] = data[index2];
+    data[index2] = temp;
 }
 
 
 
 
 export default sort
+export {swap, compareNumbers, compareStrings}
---
+++ src/utils/sortData.test.js
@@ -0,0 +1,141 @@
+/**
+ * Created by kare2436 on 2/11/17.
+ */
+import sort, {compareNumbers, compareStrings, swap} from './sortData'
+
+describe('sortData', ()=> {
+    it('should do something', () =>{
+        expect(true).toEqual(true)
+    });
+
+
+    describe('swap', () => {
+        it('should swap two values', () => {
+            let test = [1, 2];
+            swap(test, 0, 1);
+            expect(test).toEqual([2,1]);
+        });
+
+        it('should swap two values far away from each other', () => {
+            let test = [1, 2, 3, 4, 6, 7, 2, 9];
+            swap(test, 0, 7);
+            expect(test).toEqual([9, 2, 3, 4, 6, 7, 2, 1]);
+        });
+    });
+
+   describe('compareNumbers', () => {
+        it('should return false for equal numbers', () => {
+            expect(compareNumbers(1, 1)).toEqual(false)
+        });
+
+        it('should return true when first arg is greater than second arg', () => {
+            expect(compareNumbers(36, 25)).toEqual(true)
+        });
+
+       it('should return false when first arg is less than second arg', () => {
+           expect(compareNumbers(100, 1000)).toEqual(false)
+       });
+   })
+
+    describe('compareStrings', () => {
+        it('should return false for equal strings', () => {
+            expect(compareStrings("aa", "aa")).toEqual(false)
+        });
+
+        it('should return true when first arg is identical but longer than second arg', () => {
+            expect(compareStrings("aaa", "aa")).toEqual(true)
+        });
+
+        it('should return false when second arg is identical but longer than first arg', () => {
+            expect(compareStrings("aa", "aaa")).toEqual(false)
+        });
+
+        it('should return true when first arg comes after second arg lexicographically', () => {
+            expect(compareStrings("ab", "aa")).toEqual(true)
+        });
+
+        it('should return false when first arg comes before second arg lexicographically', () => {
+            expect(compareStrings("af", "ag")).toEqual(false)
+        });
+    })
+
+    describe('sort', () => {
+        it('should sort by number in data object array', () => {
+            const numItemsToGen = 1e2;
+            let data = [], expected = [];
+
+            for (let i = 0; i < numItemsToGen; i++ ) {
+                data.push({number: (numItemsToGen - 1) - i});
+                expected.push({number: i })
+            }
+
+            sort(data, 'number');
+
+            expect(data).toEqual(expected);
+        });
+
+        //TODO get better data for strings
+        it('should sort by strings in data object array', () => {
+            let data = [
+                {
+                    _string: "zb"
+                },
+
+                {
+                    _string: "za"
+                },
+
+                {
+                    _string: "bg"
+                },
+
+                {
+                    _string: "a"
+                }
+            ];
+
+
+            let expected = [
+                {
+                    _string: "a"
+                },
+
+                {
+                    _string: "bg"
+                },
+
+                {
+                    _string: "za"
+                },
+
+                {
+                    _string: "zb"
+                }
+            ];
+
+            sort(data, '_string');
+
+            expect(data).toEqual(expected);
+        });
+
+        fit('should throw error if trying to sort on unsupported type', () => {
+            let data = [
+                {
+                    _array: [],
+
+                },
+                {
+                    _array: []
+
+                }
+            ];
+
+            expect(() => {
+                sort(data, '_array')
+            }).toThrow(/This function doesn't support sorting this type of data/);
+        });
+
+    })
+
+});
+
